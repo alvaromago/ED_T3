13e88179c076713cc4d769115ea1670c61192248
alvaromago <alvaromago>
2023-02-22 11:16:00 +0100

    First commit

---
+++ src/es/studium/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,15 @@
+package es.studium.Ejercicio3;
+
+public class Ejercicio3
+{
+	public static void main(String[] args)
+	{
+		String mensaje = "Las veces que se ha ejecutado el bucle son: ";
+		int contador = 1;
+		while (contador < 100)
+		{
+			System.out.println(mensaje + contador + " veces");
+			contador++;
+		}
+	}
+}
